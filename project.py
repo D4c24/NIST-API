7bc3c5245ddf8178ba191b2537c3f146d6780649
David <callejasdavid16@gmail.com>
2022-01-27 18:50:11 +0400

    Add of URL API and first requests with parameters

--- project.py
+++ project.py
@@ -1,6 +1,13 @@
+import requests
+
+### https://www.circl.lu/services/cve-search/
+### https://cve.circl.lu/api/
+### URL example - curl -H "limit: 10" -H "cvss_modifier: above" -H "cvss_score: 6.8" -H "rejected: show" https://cve.circl.lu/api/query
 
 def run():
-    pass
+    payload = {'limit': '5', 'cvss_modifier': 'above', 'cvss_score': '6.8'}
+    r = requests.get('http://cve.circl.lu/api/query', params=payload)
+    print(r.text)
 
 if __name__ == '__main__':
-    run()
+    run()        
