d36529ec27dd4ef5fab5e62e287101d0c5740f11
David <callejasdavid16@gmail.com>
2022-02-04 15:18:56 +0400

    Add some comments to the script

--- project.py
+++ project.py
@@ -6,12 +6,14 @@ import requests, json
 ### URL example - curl -H "limit: 10" -H "cvss_modifier: above" -H "cvss_score: 6.8" -H "rejected: show" https://cve.circl.lu/api/query
 
 def run():
+    ## Header
+    ### limit: Limit the amount of vulnerabilities to return
+    ### cvss_modifier: Select the CVSS score of the CVEs related to cvss_score
+    ### cvss_score: CVSS score
     payload = {'limit': '10', 'cvss_modifier': 'above', 'cvss_score': '6.8'}
     base_url = 'http://cve.circl.lu/api/query'
-    ### become a reponse object into a string and save it in a dict.
     i = (requests.get(base_url, headers=payload)).text
     data = json.loads(i)['results']
-    #print(data.keys())
     cveID = [i['id'] for i in data]
     cveSummary = [i['summary'] for i in data]
     for cve, summary in zip(cveID, cveSummary):
