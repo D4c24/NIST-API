be1bd7689b91660c0741148c6212153bf7dc65d3
David <callejasdavid16@gmail.com>
2022-02-04 15:54:11 +0400

    parse fields cvss and cwe

--- project.py
+++ project.py
@@ -10,16 +10,20 @@ def run():
     ### limit: Limit the amount of vulnerabilities to return
     ### cvss_modifier: Select the CVSS score of the CVEs related to cvss_score
     ### cvss_score: CVSS score
-    payload = {'limit': '10', 'cvss_modifier': 'above', 'cvss_score': '6.8'}
+    payload = {'limit': '10', 'cvss_modifier': 'above', 'cvss_score': '6.8', 'rejected': 'hide'}
     base_url = 'http://cve.circl.lu/api/query'
     i = (requests.get(base_url, headers=payload)).text
     data = json.loads(i)['results']
     cveID = [i['id'] for i in data]
     cveSummary = [i['summary'] for i in data]
-    for cve, summary in zip(cveID, cveSummary):
+    cveCVSS = [i['cvss'] for i in data]
+    cveCWE = [i['cwe'] for i in data]
+    for cve, summary, cvss, cwe in zip(cveID, cveSummary, cveCVSS, cveCWE):
       print("\nCVE: ",cve)
       print("Summary: ",summary)
+      print("CVSS: ",cvss)
+      print("CWE: ",cwe)
 
 
 if __name__ == '__main__':
-    run() 
+    run()
